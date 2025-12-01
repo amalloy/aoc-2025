5da0f5c68ba45bca2fb5a70f9cd08df3de92ea13
Alan Malloy <alan@malloys.org>
2025-12-01 07:35:00 -0800

    Day 1, part 1

--- day01/src/Main.hs
+++ day01/src/Main.hs
@@ -5,16 +5,34 @@ module Main where
 
 import Control.Arrow ((&&&))
 
-type Input = [String]
+import Data.Maybe (fromMaybe)
 
-part1 :: Input -> ()
-part1 = const ()
+import Text.Regex.Applicative ((<|>), many, (=~), sym)
+import Text.Regex.Applicative.Common (decimal)
+
+data Direction = L | R deriving (Eq, Ord, Show, Enum)
+data Rotation = Rotation Direction Int deriving (Eq, Ord, Show)
+
+type Input = [Rotation]
+
+part1 :: Input -> Int
+part1 = length . filter (== 0) . map (`mod` 100) . scanl (+) 50 . map toOffset
+  where toOffset (Rotation dir magnitude) = magnitude * case dir of
+          L -> -1
+          R -> 1
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = many (rotation <* sym '\n')
+        rotation = Rotation <$> direction <*> decimal
+        direction = (L <$ sym 'L') <|> (R <$ sym 'R')
+-- The straightforward approach
+-- prepare = map rotation . lines
+--   where rotation ('L':n) = Rotation L (read n)
+--         rotation ('R':n) = Rotation R (read n)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
