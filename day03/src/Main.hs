e66290f478830f9e6cafad945821dce834001e9e
Alan Malloy <alan@malloys.org>
2025-12-08 14:28:53 -0800

    Day 3, part 2

--- day03/src/Main.hs
+++ day03/src/Main.hs
@@ -4,7 +4,7 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 import Data.Ord (Down(Down, getDown))
 
 type Joltage = Int
@@ -14,18 +14,21 @@ type Input = [Bank]
 dropLast :: Int -> [a] -> [a]
 dropLast n xs = zipWith const xs (drop n xs)
 
-maxJoltage :: Int -> Bank -> Int
+maxJoltage :: Int -> Bank -> [Joltage]
 maxJoltage _ [] = error "No batteries"
-maxJoltage 1 bank = maximum bank
-maxJoltage n bank = 10 * biggest + maxJoltage (n - 1) bank'
+maxJoltage 1 bank = [maximum bank]
+maxJoltage n bank = biggest : maxJoltage (n - 1) bank'
   where (biggest : bank') = drop skippedBatteries bank
         skippedBatteries = getDown . snd . maximum . (`zip` [Down 0, Down 1..]) $ dropLast (n - 1) bank
 
-part1 :: Input -> Int
-part1 = sum . map (maxJoltage 2)
+asInt :: [Joltage] -> Int
+asInt = read . map intToDigit
 
-part2 :: Input -> ()
-part2 = const ()
+part1 :: Input -> Int
+part1 = sum . map (asInt . maxJoltage 2)
+
+part2 :: Input -> Int
+part2 = sum . map (asInt . maxJoltage 12)
 
 prepare :: String -> Input
 prepare = map (map digitToInt) . lines
