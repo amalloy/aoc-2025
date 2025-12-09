f4a5c82d0472d737a403b8cae8f9cec9c6a1a3eb
Alan Malloy <alan@malloys.org>
2025-12-09 02:06:05 -0800

    Day 5, part 1

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -4,17 +4,31 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import Text.Regex.Applicative ((=~), sym, some)
+import Text.Regex.Applicative.Common (decimal)
 
-part1 :: Input -> ()
-part1 = const ()
+type Ingredient = Int
+data Range = Range Ingredient Ingredient deriving (Eq, Ord, Show)
+data Input = Input [Range] [Ingredient] deriving Show
+
+contains :: Range -> Ingredient -> Bool
+(Range lo hi) `contains` x = x >= lo && x <= hi
+
+part1 :: Input -> Int
+part1 (Input ranges available) = length . filter fresh $ available
+  where fresh i = any (`contains` i) ranges
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = Input <$> some range <*> (newline *> some (ingredient <* newline))
+        range = Range <$> ingredient <*> (sym '-' *> ingredient <* newline)
+        ingredient = decimal
+        newline = sym '\n'
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
