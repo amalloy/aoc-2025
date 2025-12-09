1ccfbea7075c7089ab746365324954d184342c4a
Alan Malloy <alan@malloys.org>
2025-12-09 00:35:58 -0800

    Day 4, part 1

--- day04/src/Main.hs
+++ day04/src/Main.hs
@@ -4,17 +4,38 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Control.Monad (guard)
 
-type Input = [String]
+import qualified Data.Set as S
 
-part1 :: Input -> ()
-part1 = const ()
+data Coord = Coord Int Int deriving (Eq, Ord, Show)
+instance Semigroup Coord where
+  (Coord y1 x1) <> (Coord y2 x2) = Coord (y1 + y2) (x1 + x2)
+instance Monoid Coord where
+  mempty = Coord 0 0
+
+type Input = S.Set Coord
+
+directions :: S.Set Coord
+directions = S.fromList . tail $ do
+  dy <- [0, 1, -1]
+  dx <- [0, 1, -1]
+  pure $ Coord dy dx
+
+part1 :: Input -> Int
+part1 rolls = length . S.filter accessible $ rolls
+  where accessible roll = length neighbors < 4
+          where neighbors = S.intersection rolls . S.map (roll <>) $ directions
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare text = S.fromList $ do
+  (y, row) <- zip [0..] $ lines text
+  (x, c) <- zip [0..] row
+  guard $ c == '@'
+  pure $ Coord y x
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
