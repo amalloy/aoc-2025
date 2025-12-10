4224a3f60f83f586a8cccf5f3b85bbd922edd95a
Alan Malloy <alan@malloys.org>
2025-12-10 03:02:43 -0800

    Day 6: Separate parser for part 1 and part 2

--- day06/src/Main.hs
+++ day06/src/Main.hs
@@ -20,27 +20,29 @@ apply Times = (*)
 
 data ProblemF f = ProblemF (f (ZipList Operand)) (f Operator)
 type Problem = ProblemF Identity
-type Input = ProblemF ZipList
+type Input = String
 
 solve :: Applicative f => ProblemF f -> f Int
 solve (ProblemF args ops) = go <$> args <*> ops
   where go arg op = foldl1 (apply op) arg
 
 part1 :: Input -> Int
-part1 (ProblemF args ops) = sum . solve $ ProblemF args' ops
+part1 sheet = sum . solve $ ProblemF args' ops
   where args' = sequenceA args
+        (ProblemF args ops) = parse sheet
+        parse = fromMaybe (error "no parse") . (=~ input)
+          where p `sepBy` sep = opt sep *> ((:) <$> p <*> (many (sep *> p) <* opt sep))
+                opt p = p <|> mempty
+                input = ProblemF <$> (ZipList <$> some (row arg)) <*> (row op)
+                row p = ZipList <$> ((p `sepBy` (some (sym ' '))) <* sym '\n')
+                arg = decimal
+                op = (Plus <$ sym '+') <|> (Times <$ sym '*')
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = fromMaybe (error "no parse") . (=~ input)
-  where p `sepBy` sep = opt sep *> ((:) <$> p <*> (many (sep *> p) <* opt sep))
-        opt p = p <|> mempty
-        input = ProblemF <$> (ZipList <$> some (row arg)) <*> (row op)
-        row p = ZipList <$> ((p `sepBy` (some (sym ' '))) <* sym '\n')
-        arg = decimal
-        op = (Plus <$ sym '+') <|> (Times <$ sym '*')
+prepare = id
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
