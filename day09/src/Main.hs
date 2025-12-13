e9882873a55b973979a2d20ec131ff9cfd4ef5a7
Alan Malloy <alan@malloys.org>
2025-12-12 21:23:30 -0800

    Day 9, part 1

--- day09/src/Main.hs
+++ day09/src/Main.hs
@@ -4,17 +4,30 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import Text.Regex.Applicative ((=~), some, sym)
+import Text.Regex.Applicative.Common (decimal)
 
-part1 :: Input -> ()
-part1 = const ()
+import qualified Data.Set as S
+
+data Coord = Coord Int Int deriving (Eq, Ord, Show)
+
+type Input = S.Set Coord
+
+part1 :: Input -> Int
+part1 input = maximum . map (uncurry area) $ liftA2 (,) tiles tiles
+  where tiles = S.toList input
+        area (Coord y1 x1) (Coord y2 x2) = dim y1 y2 * dim x1 x2
+        dim a b = abs (a - b) + 1
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = S.fromList <$> some (tile <* sym '\n')
+        tile = Coord <$> (decimal <* sym ',') <*> decimal
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
