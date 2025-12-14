63e9894d154859ffb91208cac335084d27c855cf
Alan Malloy <alan@malloys.org>
2025-12-13 16:59:07 -0800

    Day 10: types & parser

--- day10/src/Main.hs
+++ day10/src/Main.hs
@@ -4,8 +4,17 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import qualified Data.IntSet as S
+
+import Text.Regex.Applicative ((=~), (<|>), some, many, sym)
+import Text.Regex.Applicative.Common (decimal)
+
+type Joltage = Int
+type Button = S.IntSet
+data Machine = Machine [Bool] [Button] [Joltage] deriving Show
+type Input = [Machine]
 
 part1 :: Input -> ()
 part1 = const ()
@@ -14,7 +23,18 @@ part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = some (machine <* sym '\n')
+        machine = Machine <$>
+          (lights <* sym ' ') <*>
+          some (S.fromList <$> button <* sym ' ') <*>
+          joltages
+        lights = sym '[' *> some light <* sym ']'
+        light = (True <$ sym '#') <|> (False <$ sym '.')
+        ints open close = sym open *> (decimal `sepBy` sym ',') <* sym close
+        joltages = ints '{' '}'
+        button = ints '(' ')'
+        p `sepBy` sep = (:) <$> p <*> many (sep *> p)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
