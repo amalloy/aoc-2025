695261c5367edc3ac6773971e8d137311fe4947b
Alan Malloy <alan@malloys.org>
2025-12-13 17:40:33 -0800

    Day 10, part 1

--- day10/src/Main.hs
+++ day10/src/Main.hs
@@ -4,20 +4,38 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Control.Monad (replicateM)
+import Control.Monad.Identity (Identity(..))
 import Data.Maybe (fromMaybe)
 
 import qualified Data.IntSet as S
+import Data.IntSet (IntSet)
 
 import Text.Regex.Applicative ((=~), (<|>), some, many, sym)
 import Text.Regex.Applicative.Common (decimal)
 
 type Joltage = Int
-type Button = S.IntSet
-data Machine = Machine [Bool] [Button] [Joltage] deriving Show
+type Button = IntSet
+data Machine = Machine IntSet [Button] [Joltage] deriving Show
 type Input = [Machine]
 
-part1 :: Input -> ()
-part1 = const ()
+toInts :: [Bool] -> IntSet
+toInts = S.fromList . map fst . filter snd . zip [0..]
+
+solutions :: Machine -> [IntSet]
+solutions (Machine goal buttons _joltages) = map toInts . filter solved $ buttonChoices
+  where buttonChoices = replicateM (length buttons) [False, True]
+        solved :: [Bool] -> Bool
+        solved = (== goal) . S.foldl' press S.empty . toInts
+        press :: IntSet -> Int -> IntSet
+        press lights buttonIdx = S.foldl' toggle lights (buttons !! buttonIdx)
+        toggle :: IntSet -> Int -> IntSet
+        toggle lights lightNum = runIdentity (S.alterF invert lightNum lights)
+        invert :: Bool -> Identity Bool
+        invert b = Identity (not b)
+
+part1 :: Input -> Int
+part1 = sum . map (minimum . map S.size . solutions)
 
 part2 :: Input -> ()
 part2 = const ()
@@ -29,7 +47,7 @@ prepare = fromMaybe (error "no parse") . (=~ input)
           (lights <* sym ' ') <*>
           some (S.fromList <$> button <* sym ' ') <*>
           joltages
-        lights = sym '[' *> some light <* sym ']'
+        lights = sym '[' *> (toInts <$> some light) <* sym ']'
         light = (True <$ sym '#') <|> (False <$ sym '.')
         ints open close = sym open *> (decimal `sepBy` sym ',') <* sym close
         joltages = ints '{' '}'
