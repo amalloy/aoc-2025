c03ee9de3b21865cbb65865271ef39611f72e421
Alan Malloy <alan@malloys.org>
2025-12-15 21:05:31 -0800

    Day 12, part 1

--- day12/src/Main.hs
+++ day12/src/Main.hs
@@ -1,20 +1,46 @@
-{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Foldable (toList)
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import Text.Regex.Applicative ((=~), (<|>), many, sym)
+import Text.Regex.Applicative.Common (decimal)
 
-part1 :: Input -> ()
-part1 = const ()
+import GHC.Generics (Generic1, Generically1(..))
+
+data Three a = Three a a a deriving (Eq, Ord, Show, Foldable, Traversable, Functor, Generic1)
+  deriving Applicative via Generically1 Three
+
+newtype Present = Present (Three (Three Bool)) deriving Show
+data Problem = Problem Region [Int] deriving Show
+type Region = (Int, Int)
+data Input = Input [Present] [Problem] deriving Show
+
+slack :: [Present] -> Problem -> Int
+slack ps (Problem (h, w) cardinalities) = area - footprint
+  where area = h * w
+        footprint = sum $ zipWith (*) (map size ps) cardinalities
+        size (Present p) = length . filter id . (>>= toList) . toList $ p
+
+part1 :: Input -> Int
+part1 (Input shapes problems) = length . filter ((>= 0) . slack shapes) $ problems
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = Input <$> many (present <* sym '\n') <*> many (problem <* sym '\n')
+        present = label *> (Present <$> sequenceA (pure row))
+        row = sequenceA (pure cell) <* sym '\n'
+        cell = (True <$ sym '#') <|> (False <$ sym '.')
+        label = (decimal @Int) *> sym ':' *> sym '\n'
+        problem = Problem <$> region <*> many (sym ' ' *> decimal)
+        region = (,) <$> (decimal <* sym 'x') <*> (decimal <* sym ':')
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
