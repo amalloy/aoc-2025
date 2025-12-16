7bc33c25e4b98c6614cbe0b117a3d1ddb5ff60f7
Alan Malloy <alan@malloys.org>
2025-12-15 19:08:42 -0800

    Day 11, part 1

--- day11/src/Main.hs
+++ day11/src/Main.hs
@@ -4,17 +4,36 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List.NonEmpty (NonEmpty(..))
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import Text.Regex.Applicative ((=~), many, sym, psym)
+import qualified Data.Map.Lazy as M
 
-part1 :: Input -> ()
-part1 = const ()
+data Label = Label Char Char Char deriving (Eq, Ord, Show)
+data Node = Node Label (NonEmpty Label) deriving (Show)
+
+type Input = [Node]
+
+part1 :: Input -> Int
+part1 nodes = get you
+  where you = Label 'y' 'o' 'u'
+        out = Label 'o' 'u' 't'
+        get = (m M.!)
+        m = M.insert out 1 . M.fromList $ do
+          Node from tos <- nodes
+          pure (from, sum . fmap get $ tos)
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (error "no parse") . (=~ input)
+  where input = many (node <* sym '\n')
+        node = Node <$> (label <* sym ':') <*> some (sym ' ' *> label)
+        label = Label <$> labelChar <*> labelChar <*> labelChar
+        labelChar = psym (/= '\n')
+        some p = (:|) <$> p <*> many p
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
