059ef3e76f61fa9ab035f53b4cf55a0b4adbb64c
Alan Malloy <alan@malloys.org>
2025-12-16 02:33:47 -0800

    Day 12: un-specialize Present so we get toList for free

--- day12/src/Main.hs
+++ day12/src/Main.hs
@@ -12,10 +12,11 @@ import Text.Regex.Applicative.Common (decimal)
 
 import GHC.Generics (Generic1, Generically1(..))
 
-data Three a = Three a a a deriving (Eq, Ord, Show, Foldable, Traversable, Functor, Generic1)
+data Three a = Three a a a deriving (Show, Foldable, Traversable, Functor, Generic1)
   deriving Applicative via Generically1 Three
-
-newtype Present = Present (Three (Three Bool)) deriving Show
+newtype Nine a = Nine (Three (Three a)) deriving (Show, Foldable, Traversable, Functor, Generic1)
+  deriving Applicative via Generically1 Nine
+type Present = Nine Bool
 data Problem = Problem Region [Int] deriving Show
 type Region = (Int, Int)
 data Input = Input [Present] [Problem] deriving Show
@@ -24,7 +25,7 @@ slack :: [Present] -> Problem -> Int
 slack ps (Problem (h, w) cardinalities) = area - footprint
   where area = h * w
         footprint = sum $ zipWith (*) (map size ps) cardinalities
-        size (Present p) = length . filter id . (>>= toList) . toList $ p
+        size = length . filter id . toList p
 
 part1 :: Input -> Int
 part1 (Input shapes problems) = length . filter ((>= 0) . slack shapes) $ problems
@@ -35,7 +36,7 @@ part2 = const ()
 prepare :: String -> Input
 prepare = fromMaybe (error "no parse") . (=~ input)
   where input = Input <$> many (present <* sym '\n') <*> many (problem <* sym '\n')
-        present = label *> (Present <$> sequenceA (pure row))
+        present = label *> (Nine <$> sequenceA (pure row))
         row = sequenceA (pure cell) <* sym '\n'
         cell = (True <$ sym '#') <|> (False <$ sym '.')
         label = (decimal @Int) *> sym ':' *> sym '\n'
